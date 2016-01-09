0ee6de436794104690f347f7837e6ad9e0c4ae8c
Erin McCargar <erin.mccargar@athlonsports.com>
2016-01-08 18:24:39 -0600

    First pass at a Events controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'application#index'
 
+  resources :events
+   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
